a77ab7e958857e5e299a0b74883e11c569ed73e6
Bartlomiej Kocinski <b.kocinski@student.uw.edu.pl>
2025-02-21 00:32:25 +0100

    test sukces

---
+++ build/CMakeFiles/projekt.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/dmj/fizmed/bkocinski/geant4_projekt/projekt.cc" "CMakeFiles/projekt.dir/projekt.cc.o" "gcc" "CMakeFiles/projekt.dir/projekt.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/DetectorConstruction.cc" "CMakeFiles/projekt.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/projekt.dir/src/DetectorConstruction.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/EventAction.cc" "CMakeFiles/projekt.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/projekt.dir/src/EventAction.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/Output.cc" "CMakeFiles/projekt.dir/src/Output.cc.o" "gcc" "CMakeFiles/projekt.dir/src/Output.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/PhysicsList.cc" "CMakeFiles/projekt.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/projekt.dir/src/PhysicsList.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/PrimaryGeneratorAction.cc" "CMakeFiles/projekt.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/projekt.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/RunAction.cc" "CMakeFiles/projekt.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/projekt.dir/src/RunAction.cc.o.d"
+  "/dmj/fizmed/bkocinski/geant4_projekt/src/SteppingAction.cc" "CMakeFiles/projekt.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/projekt.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/projekt.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt.dir/projekt.cc.o"
+  "CMakeFiles/projekt.dir/projekt.cc.o.d"
+  "CMakeFiles/projekt.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/projekt.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/projekt.dir/src/EventAction.cc.o"
+  "CMakeFiles/projekt.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/projekt.dir/src/Output.cc.o"
+  "CMakeFiles/projekt.dir/src/Output.cc.o.d"
+  "CMakeFiles/projekt.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/projekt.dir/src/PhysicsList.cc.o.d"
+  "CMakeFiles/projekt.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/projekt.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/projekt.dir/src/RunAction.cc.o"
+  "CMakeFiles/projekt.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/projekt.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/projekt.dir/src/SteppingAction.cc.o.d"
+  "projekt"
+  "projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
